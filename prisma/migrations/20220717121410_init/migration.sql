56e664b42d263c6deaa6e59af0352b6b24efd366
Titux Metal (DEV) <github@lgdweb.fr>
2022-07-17 14:27:57 +0200

    Updating prisma schema; adds init migration that creates the users table

---
+++ prisma/migrations/20220717121410_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "refreshTokenHash" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
